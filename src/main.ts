59a28d8b39573e25efddb3c69c510f46215692cf
Jesse <jessebot.git@gmail.com>
2025-04-20 00:59:32 -0700

    build: replace old template with new vite typescript template

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { Boot } from './scenes/Boot';
-import { ClickerGame } from './scenes/ClickerGame';
-import { Game } from 'phaser';
+import { Game as MainGame } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
 import { Preloader } from './scenes/Preloader';
 
-//  Find out more information about the Game Config at: https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
-const config = {
+import { Game, Types } from "phaser";
+
+//  Find out more information about the Game Config at:
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config: Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 1024,
     height: 768,
@@ -16,17 +18,11 @@ const config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 400 }
-        }
-    },
     scene: [
         Boot,
         Preloader,
         MainMenu,
-        ClickerGame,
+        MainGame,
         GameOver
     ]
 };
